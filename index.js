8a8f7a6099c9d369c00d20268acc515fe7abc9e7
Tonny Bett <betttonny26@gmail.com>
2024-07-19 00:44:33 +0300

    fetch from db.json to html

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-let addBudgetButton = document.getElementById('addBudget');
-let BudgetContainer = document.getElementById('BudgetContainer');
-let inputField = document.getElementById('inputField');
+const income = 50000;
+let totalExpenses = 0;
 
-function fetchData(){
-    fetchData("http://localhost:3000/0")
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-    });
-}
+let expenseListTable = document.getElementsByClassName('expense-list-table')[0];
+let totalExpenseDisplay = document.getElementsByClassName('total-expense-display')[0];
+let expenseItem=document.getElementById('main')[0];
+
+
+fetch ("http://localhost:3000/expense")
+.then(response => response.json())
+.then(expenseItem=> {
+  expense.forEach(expense=>{
+  const title= document.createElement("h1")
+  title.innerHTML = expenseItem.title
+  main.appendChild(title);
+});
+})
+
+console.log('before calculating...')
+expenseItems.forEach(item=>{
+    console.log('calculating...')
+    return totalExpenses += item.amount;
+});
+console.log('after calculating...')
+totalExpenseDisplay.innerHTML = totalExpenses;
+console.log('after display calculating...', expenseListTable)
+
+//.join for 
+const tableRows = expenseItems.map(item => {
+    return `
+        <tr>
+            <td>${item.name}</td>
+            <td>${item.amount}</td>
+        </tr>
+    `;
+});
+expenseListTable.innerHTML = tableRows.join('');
+
+// When user submits an item, get the inputs (desc, amount, month) 
+// Add event handler for form submission
+
 addBudgetButton.addEventListener('click',function(){
     var paragraph=document.createElement('p');
 
@@ -24,3 +54,6 @@ addBudgetButton.addEventListener('click',function(){
         })
     })
 });
+
+// Update kwa DB, ama kwa expenseItems array
+ 
